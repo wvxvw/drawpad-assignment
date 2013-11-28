3cb2dd10896deba0059cb434da4efede8a9a2036
wvxvw <olegsivokon@gmail.com>
2013-11-29 01:36:29 +0200

    Server now can serve postscripts. Also fixed couple bugs related to file system navigation.

--- server/services.py
+++ server/services.py
@@ -51,19 +51,33 @@ Defaults to "example"''')
 
 (options, args) = parser.parse_args()
 
-STATIC_CONTENT = { '.html' : 'text/html',
-                   '.jpg' : 'image/jpg',
-                   '.swf' : 'application/x-shockwave-flash',
-                   '.gif' : 'image/gif',
-                   '.js' : 'application/javascript',
-                   '.css' : 'text/css' }
-# Add some services later
-SERVICES = { }
-
 class DrawpadHandler(BaseHTTPRequestHandler):
 
-    www = path.join(curdir, 'www')
-    
+    www = path.join(path.dirname(path.realpath(__file__)), '../www')
+    ps = path.join(path.dirname(path.realpath(__file__)), 'postscript')
+    statics = { '.html' : 'text/html',
+                '.jpg' : 'image/jpg',
+                '.swf' : 'application/x-shockwave-flash',
+                '.gif' : 'image/gif',
+                '.js' : 'application/javascript',
+                '.css' : 'text/css' }
+
+    def serve(self, name):
+        if name in ['square']:
+            self.serve_postscript(name)
+        else:
+            self.send_error(404, 'No Such Service')
+
+    def serve_postscript(self, name):
+        try:
+            with open(path.join(self.ps, name + '.ps'), 'r') as served:
+                self.send_response(200)
+                self.send_header('Content-type', 'application/postscript')
+                self.end_headers()
+                self.wfile.write(served.read())
+        except IOError:
+            self.send_error(404, 'Failed to create postscript: %s' % name)
+        
     def serve_static(self, file, mime):
         '''The handler for static files'''
         try:
@@ -81,14 +95,18 @@ class DrawpadHandler(BaseHTTPRequestHandler):
         extension = path.splitext(url.path)[1]
         arguments = parse_qs(url.query)
         if self.path == '/':
-            self.serve_static('drawpad.html', STATIC_CONTENT['.html'])
-        elif extension in STATIC_CONTENT:
-            self.serve_static(url.path[1:], STATIC_CONTENT[extension])
+            self.serve_static('drawpad.html', self.statics['.html'])
+        elif extension in self.statics:
+            self.serve_static(url.path[1:], self.statics[extension])
         elif 'service' in arguments:
-            try:
-                getattr(self, SERVICES[arguments['service'][0]])()
-            except KeyError:
-                self.send_error(404, 'No Such Service')
+            self.serve([arguments['service']][0][0])
+        else:
+            self.send_error(500, 'Don\'t know what to do with: %s' % [url])
+
+    def do_POST(self):
+        url = urlparse(self.path)
+        self.serve([url.path])
+        
 
 try:
     server = HTTPServer(('', options.port), DrawpadHandler)
